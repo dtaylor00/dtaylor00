6bdb1fc600ac2cfeeabf1bea0067ee19d7bd402a
Dakota Taylor <54542017+dtaylor00@users.noreply.github.com>
2024-05-15 00:07:54 -0400

    public files only

--- dist/analog/public/assets/index.page-CX0DrG4C.js
+++
@@ -1,9 +0,0 @@
-import{d as c,f as m,G as e,g as _,H as t,J as n,v as i,K as g,M as p}from"./index-By-_LwWc.js";let f=(()=>{var o;class r{constructor(){this.count=0}increment(){this.count++}}return o=r,o.ɵfac=function(a){return new(a||o)},o.ɵcmp=c({type:o,selectors:[["app-home"]],standalone:!0,features:[m],decls:16,vars:1,consts:[["href","https://analogjs.org/","target","_blank"],["alt","Analog Logo","src","/analog.svg",1,"logo","analog"],[1,"card"],["type","button",3,"click"],[1,"read-the-docs"],["href","https://analogjs.org","target","_blank"],["href","/test"]],template:function(a,l){a&1&&(e(0,"div")(1,"a",0),_(2,"img",1),t()(),e(3,"h2"),n(4,"Analog"),t(),e(5,"h3"),n(6,"The fullstack meta-framework for Angular!"),t(),e(7,"div",2)(8,"button",3),i("click",function(){return l.increment()}),n(9),t()(),e(10,"p",4),n(11," For guides on how to customize this project, visit the "),e(12,"a",5),n(13,"Analog documentation"),t()(),e(14,"a",6),n(15,"Go to test page"),t()),a&2&&(g(9),p("Count ",l.count,""))},styles:[`.logo[_ngcontent-%COMP%] {
-        will-change: filter;
-      }
-      .logo[_ngcontent-%COMP%]:hover {
-        filter: drop-shadow(0 0 2em #646cffaa);
-      }
-      .read-the-docs[_ngcontent-%COMP%] {
-        color: #888;
-      }`]}),r})();export{f as default};
